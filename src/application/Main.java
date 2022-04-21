754dc72ec6ad1848a02e305badd24e38513053af
Jamie <jamie.gillett@ucdconnect.ie>
2022-04-21 19:25:40 +0100

    set up new javaFX project

---
+++ src/application/Main.java
@@ -0,0 +1,26 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage stage) {
+		try {
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			stage.setScene(scene);
+			stage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,5 @@
+module MyAsteroids {
+	requires javafx.controls;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
