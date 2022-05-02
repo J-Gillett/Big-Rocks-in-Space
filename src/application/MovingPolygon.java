09cc2a8729461f839c959ff6d79be8221f25702c
Jamie <jamie.gillett@ucdconnect.ie>
2022-05-02 13:29:54 +0100

    Update MovingPolygon.java
    extending update()

--- src/application/MovingPolygon.java
+++ src/application/MovingPolygon.java
@@ -17,6 +17,10 @@ public class MovingPolygon extends Polygon {
 		this.setCoordinates();
 		this.position = new Point2D(100.0,100.0);
 		this.velocity = new Point2D(50.0,0.0);
+		this.acceleration = new Point2D(0.0,0.0);
+		this.heading = 0.0;
+		this.angularMomentum = 0.0;
+		this.proximityRadius = 0.0;
 		this.setFill(Color.RED);
 	}
 	
@@ -30,8 +34,13 @@ public class MovingPolygon extends Polygon {
 	}
 	
 	public void update(double dt) {
-		this.position = this.position.add(this.velocity.multiply(dt));
+		this.heading = this.heading + this.angularMomentum*dt; // change heading based on angular momentum
+		this.heading = this.heading %360; // restrict heading from 0 to 360 degrees
+		// TODO change acceleration based on heading
+		// TODO change velocity based on acceleration
+		this.position = this.position.add(this.velocity.multiply(dt)); // TODO take into account acceleration
 		this.setTranslateX(this.position.getX());
 		this.setTranslateY(this.position.getY());
+		this.setRotate(this.heading);
 	}
 }
