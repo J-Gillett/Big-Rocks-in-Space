a7a8d1c1810331a923aec0239005a5a84ec3b871
Jamie <jamie.gillett@ucdconnect.ie>
2022-05-02 14:40:37 +0100

    extending Point2D

--- src/application/MovingPolygon.java
+++ src/application/MovingPolygon.java
@@ -1,13 +1,12 @@
 package application;
 
-import javafx.geometry.Point2D;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Polygon;
 
 public class MovingPolygon extends Polygon {
-	protected Point2D position;
-	protected Point2D velocity;
-	protected Point2D acceleration;
+	protected Vector2D position;
+	protected Vector2D velocity;
+	protected Vector2D acceleration;
 	protected double heading;
 	protected double angularSpeed;
 	protected double proximityRadius;
@@ -15,11 +14,11 @@ public class MovingPolygon extends Polygon {
 	public MovingPolygon() {
 		// TESTING CRAP
 		this.setCoordinates();
-		this.position = new Point2D(100.0,100.0);
-		this.velocity = new Point2D(50.0,0.0);
-		this.acceleration = new Point2D(0.0,0.0);
+		this.position = new Vector2D(700.0,500.0);
+		this.velocity = new Vector2D(0.0,0.0);
+		this.acceleration = new Vector2D(200.0,0.0);
 		this.heading = 0.0;
-		this.angularSpeed = 360.0;
+		this.angularSpeed = 90.0;
 		this.proximityRadius = 0.0;
 		this.setFill(Color.RED);
 	}
@@ -36,11 +35,12 @@ public class MovingPolygon extends Polygon {
 	public void update(double deltaTime) {
 		this.heading = this.heading + this.angularSpeed*deltaTime; // change heading based on angular speed
 		this.heading = this.heading %360; // restrict heading from 0 to 360 degrees
-		// TODO change acceleration based on heading... extend point2D
-		Point2D deltaPos = new Point2D(this.velocity.getX(),this.velocity.getY()); // initialize change in position
+		this.acceleration = this.acceleration.atAngle(this.heading);
+		Vector2D deltaPos = new Vector2D(this.velocity.getX(),this.velocity.getY()); // initialize change in position
 		this.velocity = this.velocity.add(this.acceleration.multiply(deltaTime)); // change velocity based on acceleration
 		deltaPos = deltaPos.add(this.velocity).multiply(0.5).multiply(deltaTime); // change in position = (final velocity + initial velocity)/2 * delta time
 		this.position = this.position.add(deltaPos);
+		// TODO add screen wrap
 		this.setTranslateX(this.position.getX()); // set translation based on position
 		this.setTranslateY(this.position.getY()); // set translation based on position
 		this.setRotate(this.heading); // set rotation based on heading
---
+++ src/application/Vector2D.java
@@ -0,0 +1,34 @@
+package application;
+
+import javafx.geometry.Point2D;
+
+public class Vector2D extends Point2D {
+
+	public Vector2D(double arg0, double arg1) {
+		super(arg0, arg1);
+	}
+		
+	public Vector2D add(Vector2D point) {
+		return new Vector2D(
+				super.add(point).getX(),
+				super.add(point).getY()
+				);
+	}
+		
+	public Vector2D multiply(double factor) {
+		return new Vector2D(
+				super.multiply(factor).getX(),
+				super.multiply(factor).getY()
+				);
+	}
+	
+	public Vector2D atAngle(double angle) {
+		angle = angle %360;
+		angle = Math.toRadians(angle);
+		double length = this.magnitude();
+		double newX = length * Math.cos(angle);
+		double newY = length * Math.sin(angle);
+		return new Vector2D(newX,newY);
+	}
+
+}
